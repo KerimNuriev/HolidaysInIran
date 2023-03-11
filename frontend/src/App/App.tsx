0ad285cf2faac21b61caf83487d8147f2ac0f92c
Kerim <charim.nio@yandex.ru>
2023-03-11 12:52:26 +0300

    refactoring

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Navigation, Pagination } from 'swiper';
-
-import Layout from './Layout'
-import Home from '../features/Home/Home'
+import Layout from './Layout';
+import Home from '../features/Home/Home';
 import NotFound from '../features/NotFound/NotFound';
 import Tour8 from '../features/Tour8/Tour8';
 import Tour10 from '../features/Tour10/Tour10';
+import Admin from '../features/Admin/Admin';
 import MyTour from '../features/MyTour/MyTour';
 import Faq from '../features/Faq/Faq';
 import './App.scss';
@@ -14,28 +14,24 @@ import 'swiper/css';
 import { useAppDispatch } from '../store';
 import { loadCities } from '../features/Home/CitySwiper/citiesSlice';
 
-
-
 function App(): JSX.Element {
-
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(loadCities());
   }, [dispatch]);
 
-
   return (
     <Routes>
-      <Route element={<Layout/>}>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/tour8" element={<Tour8/>} />
-        <Route path="/tour10" element={<Tour10/>}/>
-        <Route path="/mytour" element={<MyTour/>}/>
-        <Route path="/faq" element={<Faq/>}/>
-        <Route path="/admin" />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/tour8" element={<Tour8 />} />
+        <Route path="/tour10" element={<Tour10 />} />
+        <Route path="/mytour" element={<MyTour />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/admin" element={<Admin />} />
       </Route>
-      <Route path="*" element={<NotFound/>}/>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
