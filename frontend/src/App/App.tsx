13ae67ae79c984546a7aa491c4eb671966dd8ba3
Kerim <charim.nio@yandex.ru>
2023-03-11 16:40:53 +0300

    add login

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,26 +1,44 @@
 import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Navigation, Pagination } from 'swiper';
+import { useSelector } from 'react-redux';
 import Layout from './Layout';
 import Home from '../features/home/Home';
 import NotFound from '../features/notFound/NotFound';
 import Tour8 from '../features/tour8/Tour8';
 import Tour10 from '../features/tour10/Tour10';
-import Admin from '../features/Admin/Admin';
+import Admin from '../features/admin/Admin';
 import MyTour from '../features/myTour/MyTour';
 import Faq from '../features/faq/Faq';
 import './App.scss';
 import 'swiper/css';
+import type { RootState } from '../store';
 import { useAppDispatch } from '../store';
 import { loadCities } from '../features/cities/citiesSlice';
 import { loadTours } from '../features/tours/toursSlice';
+import { getAdmin } from '../features/admin/adminSlice';
 
 function App(): JSX.Element {
   const dispatch = useAppDispatch();
+  const authChecked = useSelector(
+    (state: RootState) => state.admin.authChecked,
+  );
 
   useEffect(() => {
     dispatch(loadCities());
   }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(getAdmin());
+  }, [dispatch]);
+
+  if (!authChecked) {
+    return (
+      <div className="spinner-border text-primary" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    );
+  }
 
   return (
     <Routes>
--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -3,14 +3,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
 import citiesReducer from './features/cities/citiesSlice';
 import daysReducer from './features/days/daysSlice';
-import toursReducer from './features/tours/toursSlice'
-
+import toursReducer from './features/tours/toursSlice';
+import adminReducer from './features/admin/adminSlice';
 
 const store = configureStore({
   reducer: {
     // ключ - название раздела (feature)
     // значение - сам редьюсер
     // у каждого раздела сайт свой собственный редьюсер
+    admin: adminReducer,
     citySwiper: citiesReducer,
     day8Swiper: daysReducer,
     tourSwiper: toursReducer,
