6b0621639271013ee58152d2597fe2b458730e73
Kerim <charim.nio@yandex.ru>
2023-03-13 22:33:59 +0300

    123

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -5,6 +5,7 @@ import Layout from './Layout';
 import Home from '../features/home/Home';
 import NotFound from '../features/notFound/NotFound';
 import Tour from '../features/tour/Tour';
+import Tour10 from '../features/tour10/Tour10';
 import Admin from '../features/admin/Admin';
 import MyTour from '../features/myTour/MyTour';
 import Faq from '../features/faq/Faq';
@@ -16,13 +17,9 @@ import { loadCities } from '../features/cities/citiesSlice';
 import { loadTours } from '../features/tours/toursSlice';
 import { getAdmin } from '../features/admin/adminSlice';
 import Contact from '../features/contact/Contact';
-import Account from '../features/account/Account';
-import { loadDays } from '../features/days/daysSlice';
-import { loadPhotos } from '../features/photo/photosSlice';
 
 function App(): JSX.Element {
   const dispatch = useAppDispatch();
-  const admin = useSelector((state: RootState) => state.admin.admin);
   const authChecked = useSelector(
     (state: RootState) => state.admin.authChecked,
   );
@@ -30,13 +27,11 @@ function App(): JSX.Element {
   useEffect(() => {
     dispatch(loadCities());
     dispatch(loadTours());
-    dispatch(loadDays());
-    dispatch(loadPhotos());
   }, [dispatch]);
 
   useEffect(() => {
     dispatch(getAdmin());
-  }, [dispatch, authChecked, admin]);
+  }, [dispatch, authChecked]);
 
   if (!authChecked) {
     return (
@@ -52,10 +47,10 @@ function App(): JSX.Element {
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/tour/:id" element={<Tour />} />
-        <Route path="/tour/mytour" element={<MyTour />} />
+        <Route path="/tour10" element={<Tour10 />} />
+        <Route path="/mytour" element={<MyTour />} />
         <Route path="/faq" element={<Faq />} />
         <Route path="/admin" element={<Admin />} />
-        {admin && <Route path="/account" element={<Account />} />}
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
